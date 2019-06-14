d9dadf3509e9406d16e4ea7bca72f6af28255990
vineet-cyberkan <vineet1.st@gmail.com>
2019-06-14 16:11:19 +0530

    call a component as a element or as a tag

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  //selector: 'app-server',
-  selector: '[app-server]',
+  //selector: 'app-server', //as a html element tag
+  selector: '[app-server]', //as a html element attribute
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
